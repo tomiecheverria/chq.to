28739a5a1b06ed2b2374bd3bc04ad3376893eb9f
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-11-28 12:44:55 -0300

    Add HomeController, HomeHelper, and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  root to: "home#index"
   
+
+#   devise_for :users, path: 'auth', path_names: {
+#   sign_in: 'login',
+#   sign_out: 'logout',
+#   password: 'secret',
+#   confirmation: 'verification',
+#   unlock: 'unblock',
+#   registration: 'register',
+#   sign_up: 'signup'
+# }
 
   # Defines the root path route ("/")
   # root "posts#index"
