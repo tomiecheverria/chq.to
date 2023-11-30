91ac015d7bc26a2947f08dae79f7d1fa19bd7930
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-11-29 21:21:18 -0300

    Add new user creation functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
+# app/models/user.rb
 class User < ApplicationRecord
   include Clearance::User
 
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
+
+  def self.create_with_params(user_params)
+    user = new(user_params)
+    user.save
+    user
+  end
 end
