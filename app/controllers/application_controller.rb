41b03b1259aad52e3dc90592c2ae9e54f6b7c462
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-02 22:06:01 -0300

    Refactor application controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
-# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-      # Otros parámetros fuertes que puedas necesitar
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
-  
+end
