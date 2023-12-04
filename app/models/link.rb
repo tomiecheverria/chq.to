fcd0ca605b71db2b189082fafb5b44b0be9e3cda
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-03 21:28:32 -0300

    Refactor link model validations and generate unique slug value

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,18 +1,20 @@
 class Link < ApplicationRecord
-  extend FriendlyId
-  friendly_id :slug_candidates, use: :slugged
-
   belongs_to :user
 
-  validates :name, presence: true
-  validates :url, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  before_validation :generate_unique_slug
 
   private
 
-  def slug_candidates
-    [
-      :name,
-      %i[name user_id]
-    ]
+  def generate_unique_slug
+    self.slug ||= generate_unique_slug_value
+  end
+
+  def generate_unique_slug_value
+    loop do
+      slug_candidate = SecureRandom.urlsafe_base64(5)
+      break slug_candidate unless self.class.exists?(slug: slug_candidate)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_03_182647) do
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "links", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "slug"
