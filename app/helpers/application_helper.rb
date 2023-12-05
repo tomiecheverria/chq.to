9aa220aad7af484a52f87588e26939c82b5b2c48
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-05 11:34:07 -0300

    Fix link URL in user show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def build_link_url(slug)
+    if Rails.env.development?
+      "http://127.0.0.1:3000/l/#{slug}"
+    else
+      "https://chq.to/l/#{slug}"
+    end
+  end
 end
