2f69b3b3e0f1ff00c7fdbb7830688ae442d07b1c
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-08 11:30:38 -0300

    Add link_form.js controller for managing link form behavior

---
+++ app/javascript/controllers/link_form.js
@@ -0,0 +1,26 @@
+// app/assets/javascripts/link_form.js
+
+document.addEventListener('turbo:load', function () {
+    console.log("El archivo link_form.js se está ejecutando correctamente.");
+    const linkTypeSelect = document.getElementById('link_type_select');
+    const expirationDateField = document.getElementById('expiration-date-field');
+    const passwordField = document.getElementById('password-field');
+    const passwordConfirmationField = document.getElementById('password-confirmation-field');
+  
+    function toggleExpirationDateField() {
+      expirationDateField.style.display = linkTypeSelect.value === 'temporary' ? 'block' : 'none';
+    }
+  
+    function togglePasswordField() {
+      passwordField.style.display = linkTypeSelect.value === 'private_link' ? 'block' : 'none';
+      passwordConfirmationField.style.display = linkTypeSelect.value === 'private_link' ? 'block' : 'none';
+    }
+  
+    linkTypeSelect.addEventListener('change', toggleExpirationDateField);
+    linkTypeSelect.addEventListener('change', togglePasswordField);
+  
+    // Llama a las funciones una vez al cargar la página para establecer el estado inicial
+    toggleExpirationDateField();
+    togglePasswordField();
+  });
+  
