857713fdd44ae7079b6598bd674906908f6a6986
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-09 18:50:06 -0300

    Refactor links controller and helper methods

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,55 +1,38 @@
 class LinksController < ApplicationController
+  include LinksHelper
   before_action :authenticate_user!
+  before_action :find_link, only: [:show, :edit, :update, :destroy]
+
   def index
     @links = current_user.links
   end
 
   def new
-    @link ||= current_user.links.build
-  end
-
-  def show
-    @link = Link.find(params[:id])
-
+    @link = build_new_link(current_user)
   end
 
   def create
-    @link = build_link_with_type(link_params)
-    if @link.save
-      redirect_to user_path(current_user), notice: 'Link successfully created.'
-    else
-      flash[:errors] = @link.errors.full_messages
-      redirect_to new_link_path(@link)
-    end
+    @link = build_link_with_type(current_user, link_params)
+    save_or_redirect(@link, 'Link successfully created.', user_path(current_user), new_link_path)
   end
 
-  def edit
-    @link = current_user.links.find(params[:id])
-  end
+  def show; end
+
+  def edit; end
 
   def update
-    @link = current_user.links.find(params[:id])
-
-    if @link.update(link_params)
-      flash[:notice] = 'Link successfully updated.'
-    else
-      flash[:errors] = @link.errors.full_messages
-    end
-    redirect_to edit_link_path(@link)
+    update_or_redirect(@link, 'Link successfully updated.', user_path(current_user), edit_link_path(@link))
   end
 
   def destroy
-    @link = current_user.links.find(params[:id])
     @link.destroy
-    flash[:notice] = 'Link successfully deleted.'
-    redirect_to user_path(current_user), notice: 'Link successfully created.'
+    redirect_to_with_notice('Link successfully deleted.', user_path(current_user))
   end
 
   def redirect
     @link = Link.find_by(slug: params[:slug])
-
     if @link
-      handle_valid_link
+      handle_valid_link(@link)
     else
       handle_invalid_link
     end
@@ -57,70 +40,20 @@ class LinksController < ApplicationController
 
   def handle_private_link_form
     @link = Link.find(params[:id])
-    if password_matches?
-      redirect_to_link
-    else
-      flash[:alert] = 'Invalid password'
-      redirect_to private_link_form_link_path(id: @link.id) , data: { turbo: false }
-    end
+    password_matches?(params[:password], @link) ? redirect_to_link(@link) : handle_invalid_password(@link)
   end
 
   def private_link_form
-    p "paso por private_link_form"
     @link = Link.find(params[:id])
   end
 
   private
 
-  def handle_valid_link
-    if @link.temporary? && @link.expired?
-      handle_expired_link
-    elsif @link.custom_private_link?
-      redirect_to private_link_form_link_path(id: @link.id) , data: { turbo: false }
-    else
-      redirect_to_link
-    end
-  end
-
-  def password_matches?
-    return false unless params[:password].present?
-  
-    params[:password] == @link.password
-  end
-
-  def handle_expired_link
-    flash[:alert] = 'The link has expired'
-    render 'errors/not_found', status: :not_found
-  end
-
-  def redirect_to_link
-    redirect_to @link.url, status: :moved_permanently, allow_other_host: true
-  end
-
-  def handle_invalid_link
-    flash[:alert] = 'This link does not exist'
-    redirect_back fallback_location: root_path
-  end
-
-  def build_link_with_type(params)
-    link = current_user.links.build(params)
-    link.link_type = link_type_from_params(params[:link_type])
-    link.password_confirmation = params[:password_confirmation] if link.private_link?
-    link
-  end
-
-  def link_type_from_params(type_param)
-    type_mapping = {
-      'temporary' => :temporary,
-      'regular' => :regular,
-      'private_link' => :private_link,
-      'ephemeral' => :ephemeral
-    }
-    type_mapping[type_param]
+  def find_link
+    @link = Link.find(params[:id])
   end
 
   def link_params
     params.require(:link).permit(:url, :expiration_date, :link_type, :password, :password_confirmation, :accessed)
   end
-
 end
--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,74 @@
 module LinksHelper
+  def save_or_redirect(link, notice, success_path, failure_path)
+    if link.save
+      redirect_to_with_notice(notice, success_path)
+    else
+      flash[:errors] = link.errors.full_messages
+      redirect_to failure_path
+    end
+  end
+
+  def update_or_redirect(link, notice, success_path, failure_path)
+    if link.update(link_params)
+      redirect_to_with_notice(notice, success_path)
+    else
+      flash[:errors] = link.errors.full_messages
+      redirect_to failure_path
+    end
+  end
+
+  def build_new_link(user)
+    user.links.build
+  end
+
+  def redirect_to_with_notice(notice, path)
+    flash[:notice] = notice
+    redirect_to path
+  end
+
+  def handle_valid_link(link)
+    if link.temporary? && link.expired?
+      handle_expired_link
+    elsif link.custom_private_link?
+      redirect_to private_link_form_link_path(id: link.id), data: { turbo: false }
+    else
+      redirect_to_link(link)
+    end
+  end
+
+  def password_matches?(password, link)
+    return false unless password.present?
+    password == link.password
+  end
+
+  def redirect_to_link(link)
+    redirect_to link.url, status: :moved_permanently, allow_other_host: true
+  end
+
+  def handle_invalid_link
+    flash[:alert] = 'This link does not exist'
+    redirect_back fallback_location: root_path
+  end
+
+  def handle_invalid_password(link)
+    flash[:alert] = 'Invalid password'
+    redirect_to private_link_form_link_path(id: link.id), data: { turbo: false }
+  end
+
+  def build_link_with_type(user, params)
+    link = user.links.build(params)
+    link.link_type = link_type_from_params(params[:link_type])
+    link.password_confirmation = params[:password_confirmation] if link.private_link?
+    link
+  end
+
+  def link_type_from_params(type_param)
+    type_mapping = {
+      'temporary' => :temporary,
+      'regular' => :regular,
+      'private_link' => :private_link,
+      'ephemeral' => :ephemeral
+    }
+    type_mapping[type_param]
+  end
 end
