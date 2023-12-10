569abf588ab9f36e47939e764980c1d07c83afac
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-10 20:08:44 -0300

    Add flash_class method to ApplicationHelper and update flash messages in application layout and devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     base_url = Rails.env.development? ? "http://127.0.0.1:3000" : "https://chq.to"
     "#{base_url}/l/#{slug}"
   end
+
+  def flash_class(type)
+    case type
+    when 'notice' then 'alert-success'
+    when 'alert'  then 'alert-warning'
+    when 'error'  then 'alert-danger'
+    else 'alert-secondary'
+    end
+  end
 end
