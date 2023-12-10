7983169893a701f0d03ff860ecdfb5bd468bf1dd
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-10 17:11:53 -0300

    Add validations to Visit model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,9 @@
+# app/models/visit.rb
 class Visit < ApplicationRecord
   belongs_to :link
+
+  validates :link_id, presence: true
+  validates :accessed_at, presence: true
+  validates :ip_address, presence: true, format: { with: /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/,
+                                                   message: 'should be a valid IP address' }
 end
