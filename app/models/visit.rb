0ec68a4126742b922fdb6f19733616e53843babd
tomas <tomas.echeverria172939@alumnos.info.unlp.edu.ar>
2023-12-10 17:42:37 -0300

    Add validation for accessed_at datetime format in Visit model

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,6 +4,17 @@ class Visit < ApplicationRecord
 
   validates :link_id, presence: true
   validates :accessed_at, presence: true
+  validate :valid_accessed_at_format
   validates :ip_address, presence: true, format: { with: /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/,
                                                    message: 'should be a valid IP address' }
+
+  private
+
+  def valid_accessed_at_format
+    formatted_date = accessed_at.strftime('%Y-%m-%d %H:%M:%S')
+    return if formatted_date.match?(/\A\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\z/)
+
+    errors.add(:accessed_at,
+               'should be a valid datetime format (YYYY-MM-DD HH:MM:SS)')
+  end
 end
